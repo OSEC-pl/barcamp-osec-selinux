97d76cbafa8e471c282e61c5b3ed3cb3ebd48ae3
Radosław Kujawa <radoslaw.kujawa@c0ff33.net>
2017-09-15 12:38:18 +0200

    Add simple login/pass form.

--- webroot/app.lua
+++ webroot/app.lua
@@ -4,6 +4,24 @@ local Cadet = require("cadet")
 
 App = {}
 
+function App.secret(env, headers, query)
+  local res = Cadet.response
+  local write = Cadet.write
+
+  res.headers["Content-Type"] = "text/html"
+  res.status = 200
+
+  write("<h1>ze secret!: </h1>")
+
+  if query["user"] == "john" and query["pass"] == "n00b" then
+     write("this is the secret I've been looking for")
+  else
+     write("wrong login or pass")
+  end
+
+  return Cadet.finish()
+end
+
 function App.index(env, headers, query)
   local res = Cadet.response
   local write = Cadet.write
@@ -17,4 +35,5 @@ function App.index(env, headers, query)
 end
 
 httpd.register_handler("index", App.index)
+httpd.register_handler("secret", App.secret)
 
